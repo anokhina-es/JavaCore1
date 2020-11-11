6340dd8a574d23af3e7c74cad9fbd480c3fa1b29
anokhina-es <anokhina.yevgeniya@gmail.com>
2020-11-12 00:40:59 +0300

    first commit

--- src/Day2/Task3.java
+++ src/Day2/Task3.java
@@ -2,16 +2,18 @@ package Day2;
 
 import java.util.Scanner;
 
-public class Task2 {
+public class Task3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Введите числа: ");
         int a = s.nextInt();
         int b = s.nextInt();
         if(a<b) {
-            for (int i = a + 1;i<b;i++) {
+            int i = a+1;
+            while (i<b) {
                 if(i>10 && i%5==0 && i%10!=0) {
                     System.out.print(i+" ");
+                    i+=1;
                 }
             }
         }
