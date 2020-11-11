402461c1b0724651d998f2c19c64bc46f16b0d38
anokhina-es <anokhina.yevgeniya@gmail.com>
2020-11-12 01:00:48 +0300

    first commit

---
+++ src/Day2/Task4.java
@@ -0,0 +1,20 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите x: ");
+        double x = s.nextDouble();
+        double y;
+        if(x>=5){
+            y = (x*x - 10)/(x+7);
+        }
+        else if((x+3>0) && (x<5)){
+           y = (x+3)*(x*x-2);
+        }
+        else y=420;
+        System.out.println("x = "+x+"; y= "+y);
+    }
+}
