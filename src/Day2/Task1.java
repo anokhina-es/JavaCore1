5315dfceb404d8a22ceec1f67c5db53ab4262f3d
anokhina-es <anokhina.yevgeniya@gmail.com>
2020-11-11 23:03:49 +0300

    first commit

--- src/Day2/Task1.java
+++ src/Day2/Task1.java
@@ -1,7 +1,26 @@
 package Day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+            Scanner g = new Scanner(System.in);
+            System.out.println("Введите количество этажей: ");
+            int x = g.nextInt();
+            if(x>=1 && x<=4) {
+                System.out.println("Малоэтажный дом");
+            }
+            else if(x>=5 && x<=8) {
+                System.out.println("Среднеэтажный дом");
+            }
+            else if(x>=9){
+                System.out.println("Многоэтажный дом");
+            }
+            else if(x<0) {
+                System.out.println("Ошибка ввода");
+            }
 
     }
+
 }
+
