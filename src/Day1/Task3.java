ca4280f9abdf26bfb2e71c6b4d31a3bb8212fa93
anokhina-es <anokhina.yevgeniya@gmail.com>
2020-11-11 17:50:56 +0300

    first commit

--- src/Day1/Task3.java
+++ src/Day1/Task3.java
@@ -2,9 +2,9 @@ package Day1;
 
 public class Task3 {
     public static void main(String[] args) {
-    String s = "JAVA";
-    for(int i=1;i<11;i++){
-        System.out.println(s);
-        }
+        String s = "JAVA";
+        for(int i=1;i<11;i++){
+            System.out.println(s);
+            }
     }
 }
